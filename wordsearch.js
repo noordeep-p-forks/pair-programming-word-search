2652cadd8082088b9d3292b14c8be2621f8a1de0
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-19 20:06:02 -0400

    implement function to match words vertically and horizontally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const getColumn = function(letters) {
+  let index = -1;
+  const coloumnArr = [];
+  let coloumn = [];
+  while (index < letters.length) {
+    const getColumn = () => letters.map((row) => {
+      coloumn.push(row[index]);
+    });
+    index += 1;
+    coloumnArr.push(coloumn.join(''));
+    coloumn = [];
+    getColumn();
+  }
+  return coloumnArr.slice(1, coloumnArr.length - 1);
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (word === "") return false;
+
+  const verticalJoin = getColumn(letters);
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  for (const words of horizontalJoin) {
+    if (words.includes(word)) return true;
+  }
+  for (const words of verticalJoin) {
+    if (words.includes(word)) return true;
+  } return false;
+
+};
+
+module.exports = wordSearch;
